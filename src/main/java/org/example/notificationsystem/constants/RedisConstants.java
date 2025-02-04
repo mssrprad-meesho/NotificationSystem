2b2b10c134125b43d39ebc5f16213c9b3323bd62
Malladi Pradyumna <sai.malladi@meesho.com>
2025-02-04 18:52:13 +0530

    dfs:

--- src/main/java/org/example/notificationsystem/constants/RedisConstants.java
+++
@@ -1,5 +0,0 @@
-package org.example.notificationsystem.constants;
-
-public final class RedisConstants {
-    public static final String blacklisted_key = "blacklisted";
-}
--- src/main/java/org/example/notificationsystem/constants/Time.java
+++
@@ -1,7 +0,0 @@
-package org.example.notificationsystem.constants;
-
-import java.time.ZoneId;
-
-public class Time {
-    public static ZoneId INDIA_ZONE_ID = ZoneId.of("Asia/Kolkata");
-}
